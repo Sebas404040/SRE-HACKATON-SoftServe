9165b58436f9aa375121b2a654372b0ed4f06167
Sebas404040 <delgadopalitogarbage@gmail.com>
2026-04-08 21:38:39 -0500

    feat: :bricks: Components layed on App component
    Components layed on App component and displayed in main component
    
    none

--- frontend/SRE-Hackaton-Proyect/src/App.jsx
+++ frontend/SRE-Hackaton-Proyect/src/App.jsx
@@ -1,8 +1,29 @@
+import { useState } from 'react';
+import { Header } from './components/Header';
+import { IncidentForm } from './components/IncidentForm';
+import { ResultAlert } from './components/ResultAlert';
+import './index.css';
+
 function App() {
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
 
   return (
-    <div className="App"></div>
-  )
+    <div className="sre-container">
+      <Header />
+      
+      <div className="main-content">
+        <ResultAlert error={error} result={result} />
+      </div>
+
+      <div className="bottom-input-container">
+        <IncidentForm 
+          onResult={setResult} 
+          onError={setError} 
+        />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
